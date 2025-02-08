9a7ef3d8e2e083de06d8702001bb9a7db0fa384e
azure0000000 <82611633+azure0000000@users.noreply.github.com>
2025-02-08 17:38:52 +0530

    update

---
+++ bberp/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+namespace BBERP.Controllers
+{
+    public class TestController : Controller
+    {
+        [Authorize(Roles = "CountryMaster")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Currency")]
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
